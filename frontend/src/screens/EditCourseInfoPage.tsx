e5b2de3a20ad5661d9b5f7a5c2f1a09bee0231eb
Bartosz Kowalski <01168892@pw.edu.pl>
2025-01-12 16:31:31 +0100

    EditCourseInfoPage CSS + respo

--- frontend/src/screens/EditCourseInfoPage.tsx
+++ frontend/src/screens/EditCourseInfoPage.tsx
@@ -5,6 +5,7 @@ import { formatAmount, formatDateTimeLocal, formatDateToBackend, intToPrice, pri
 import { CURRENCIES, LANGUAGES, MEDIA_URL } from '../constants';
 import { useParams } from 'react-router-dom';
 import TextEditor from '../components/TextEditor';
+import "../styles/EditCourseInfoPage.css";
 
 type Params = {
     id: string;
@@ -109,68 +110,101 @@ export default function EditCourseInfoPage() {
     }
 
     return (
-        <>
-            <a href="/courses/my">Back to my courses</a>
-            <br />
-            <a href={`/course/${id}/view`}>View course as user</a>
-            <br />
-            <a href={`/course/${id}/edit`}>Edit course content</a>
-            <br />
-            <a href={`/course/${id}/edit/topics`}>Edit course topics</a>
-            <br />
-            <a href={`/course/${id}/edit/members`}>Edit gifted course members</a>
-            <br />
-            <h1>Edit course information</h1>
-            Name: <input type="text" value={name} onChange={(e) => setName(e.target.value)} /> <br />
-            Description: <TextEditor value={description} onChange={(value) => setDescription(value)} /> <br />
-            Image: <img src={MEDIA_URL + course.image} id="course-image" /> <br />
-            Upload new image:
-            <input type="file" accept="image/*" onChange={handleFileChange} /> <br />
-            Language:
-            <select value={language} onChange={(e) => setLanguage(e.target.value)}>
-                {LANGUAGES.map(([code, name, flag]) => (
-                    <option key={code} value={code}>
-                        {flag}&nbsp;{name}
-                    </option>
-                ))}
-            </select> <br />
-            Duration: <input type="number" value={duration} onChange={(e) => setDuration(parseInt(e.target.value))} />
-            <br />
-            public/private:
-            <select value={isPublic.toString()} onChange={(e) => setIsPublic(e.target.value === "true")}>
-                <option value="false">Private</option>
-                <option value="true">Public</option>
-            </select>
-            <br />
-            currency:
-            <select value={priceCurrency} onChange={(e) => setPriceCurrency(e.target.value)}>
-                {CURRENCIES.map(([code, flag]) => (
-                    <option key={code} value={code}>
-                        {flag}&nbsp;{code}
-                    </option>
-                ))}
-            </select>
-            <br />
-            regular price: <input type="text" value={intToPrice(price)} onChange={(e) => setPrice(priceToInt(e.target.value))} />
-            <br />
-            enable promo?
-            <select value={enablePromo.toString()} onChange={(e) => setEnablePromo(e.target.value === "true")}>
-                <option value="false">No</option>
-                <option value="true">Yes</option>
-            </select>
-            <br />
+        <div id="edit-course-info-main">
+            <a href="/courses/my">
+                <button className="edit-course-info-button">
+                    Back to my courses
+                </button>
+            </a>
+            <a href={`/course/${id}/view/info`}>
+                <button className="edit-course-info-button">
+                    View course as user
+                </button>
+            </a>
+            <a href={`/course/${id}/edit`}>
+                <button className="edit-course-info-button">
+                    Edit course content
+                </button>
+            </a>
+            <a href={`/course/${id}/edit/topics`}>
+                <button className="edit-course-info-button">
+                    Edit course topics
+                </button>
+            </a>
+            <a href={`/course/${id}/edit/members`}>
+                <button className="edit-course-info-button">
+                    Edit gifted course members
+                </button>
+            </a>
+            <div className="edit-course-info-header">Edit course information</div>
+            <div className="edit-course-info-label-box">
+                Name:&nbsp;<input className="edit-course-info-input-text" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            </div>
+            <div className="edit-course-info-label-box">
+                Description:&nbsp;
+            </div>
+            <TextEditor value={description} onChange={(value) => setDescription(value)} /> <br />
+            <div className="edit-course-info-label-box">
+                Image:&nbsp;
+            </div>
+            <img className="edit-course-info-image" src={MEDIA_URL + course.image} id="course-image" /> <br />
+            <div className="edit-course-info-label-box">
+                Upload new image:&nbsp;<input type="file" accept="image/*" onChange={handleFileChange} />
+            </div>
+            <div className="edit-course-info-label-box">
+                Language:&nbsp;
+                <select className="edit-course-info-select" value={language} onChange={(e) => setLanguage(e.target.value)}>
+                    {LANGUAGES.map(([code, name, flag]) => (
+                        <option key={code} value={code}>
+                            {flag}&nbsp;{name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div className="edit-course-info-label-box">
+                Duration:&nbsp;<input className="edit-course-info-input-number" min={1} type="number" value={duration} onChange={(e) => setDuration(parseInt(e.target.value))} /> h
+            </div>
+            <div className="edit-course-info-label-box">
+                Course visibility:&nbsp;
+                <select className="edit-course-info-select" value={isPublic.toString()} onChange={(e) => setIsPublic(e.target.value === "true")}>
+                    <option value="false">Private</option>
+                    <option value="true">Public</option>
+                </select>
+            </div>
+            <div className="edit-course-info-label-box">
+                Price currency:&nbsp;
+                <select className="edit-course-info-select" value={priceCurrency} onChange={(e) => setPriceCurrency(e.target.value)}>
+                    {CURRENCIES.map(([code, flag]) => (
+                        <option key={code} value={code}>
+                            {flag}&nbsp;{code}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div className="edit-course-info-label-box">
+                Regular price:&nbsp;<input className="edit-course-info-input-number" type="text" value={intToPrice(price)} onChange={(e) => setPrice(priceToInt(e.target.value))} />
+            </div>
+            <div className="edit-course-info-label-box">
+                Enable promo:&nbsp;
+                <select className="edit-course-info-select" value={enablePromo.toString()} onChange={(e) => setEnablePromo(e.target.value === "true")}>
+                    <option value="false">No</option>
+                    <option value="true">Yes</option>
+                </select>
+            </div>
             {enablePromo ?
                 <>
-                    promo price: <input type="text" value={intToPrice(promoPrice)} onChange={(e) => setPromoPrice(priceToInt(e.target.value))} />
-                    <br />
-                    promo expires: <input type="datetime-local" value={formatDateTimeLocal(new Date(promoExpires))} onChange={(e) => setPromoExpires(new Date(e.target.value))} />
-                    <br />
+                    <div className="edit-course-info-label-box">
+                        Promo price:&nbsp;<input className="edit-course-info-input-number" type="text" value={intToPrice(promoPrice)} onChange={(e) => setPromoPrice(priceToInt(e.target.value))} />
+                    </div>
+                    <div className="edit-course-info-label-box">
+                        Promo expires:&nbsp;<input type="datetime-local" value={formatDateTimeLocal(new Date(promoExpires))} onChange={(e) => setPromoExpires(new Date(e.target.value))} />
+                    </div>
                 </>
                 : ""}
             <br />
-            <button type="button" onClick={handleEditCourse}>Save changes</button>
+            <button className="edit-course-info-button" type="button" onClick={handleEditCourse}>Save changes</button>
             <br /><br /><br /><br />
 
-        </>
+        </div>
     )
 };
