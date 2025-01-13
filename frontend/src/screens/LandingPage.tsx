335fac96ca1390b0953a4b0f795d3c903441d753
Bartosz Kowalski <01168892@pw.edu.pl>
2025-01-13 22:26:56 +0100

    LandingPage CSS + respo

--- frontend/src/screens/LandingPage.tsx
+++ frontend/src/screens/LandingPage.tsx
@@ -1,13 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import '../App.css';
+import "../styles/LandingPage.css";
 
 export default function LandingPage() {
     return (
-        <>
-            <h1>Hello</h1>
-            <a href="/login">Login</a>
-            <br />
-            <a href="/register">Register</a>
-        </>
+        <div id="landing-page-main-container">
+            <div id="landing-page-header">
+                Hello,<br />
+                Welcome to <span className="landing-page-app-name">LearningApp</span>!
+            </div>
+            <div id="landing-page-sub-header">
+                Try our users' courses!
+                <a href="/explore">
+                    <button className="landing-page-button">Explore</button>
+                </a>
+            </div>
+            <div className="landing-page-auth">
+                Already have an account?
+                <br />
+                <a href="/login">
+                    <button className="landing-page-button">
+                        Login
+                    </button>
+                </a>
+            </div>
+            <div className="landing-page-auth">
+                Don't have an account?
+                <br />
+                <a href="/register">
+                    <button className="landing-page-button">
+                        Register
+                    </button>
+                </a>
+            </div>
+        </div>
     );
 };
