bbe43d1ee9e0d9956983cfa37cfa410cd3d35b1e
Bartosz Kowalski <01168892@pw.edu.pl>
2025-01-13 22:40:43 +0100

    LearnPage CSS + respo

--- frontend/src/screens/LearnPage.tsx
+++ frontend/src/screens/LearnPage.tsx
@@ -4,6 +4,7 @@ import "../types";
 import { formatAmount, formatDateTimeLocal, formatDateToBackend, intToPrice, priceToInt, sendUserBackToLoginPageIfNotLoggedIn, timeAgo } from '../functions';
 import { CURRENCIES, LANGUAGES, MEDIA_URL } from '../constants';
 import { useParams } from 'react-router-dom';
+import "../styles/LearnPage.css";
 
 
 export default function LearnPage() {
@@ -37,42 +38,47 @@ export default function LearnPage() {
     }
 
     return (
-        <div id="main-container">
-            <h1>Learn</h1>
-            <div id="header">
-                <div id="header-left">
-                    Search by course name
-                    <input
-                        type="text"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        placeholder="Search"
-                        className="search-input"
-                    />
-                </div>
-                <div id="header-right">
-                </div>
+        <div id="learn-main-container">
+            <div id="learn-header">
+                <h1>Learn</h1>
+                <br />
+                Search by course name
+                <input
+                    type="text"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    placeholder="Search"
+                    className="learn-search-input"
+                />
             </div>
-            <div id="courses-container">
+            <div id="learn-courses-container">
                 {courseAccesses.map((ca) => ca.course).filter((course) => course.name.includes(searchQuery) && course.is_public).map((course) => (
                     <a href={`/course/${course.id}/view/info`}>
-                        <div className="course">
-                            <div className="course-img-container">
-                                <img
-                                    className="course-img"
-                                    src={MEDIA_URL + course.image}
-                                />
+                        <div className="learn-course">
+                            <div className="learn-course-img-container">
+                                {course.image ? (
+                                    <img
+                                        className="learn-course-img"
+                                        src={MEDIA_URL + course.image}
+                                    />
+                                ) : (
+                                    <div className="learn-img-alt">
+                                        No image
+                                    </div>
+                                )}
                             </div>
-                            <span className="course-name">
+                            <span className="learn-course-name">
                                 {course.name}
                             </span>
                             <br />
-                            <span className="course-minor-text">
-                                <img className="avatar-mini" src={MEDIA_URL + (course.author.avatar ? course.author.avatar : "/media/default_avatar.png")} />
-                                {course.author.user.username} <br />
+                            <span className="learn-minor-text">
+                                <div className="learn-author">
+                                    <img className="learn-avatar-mini" src={MEDIA_URL + (course.author.avatar ? course.author.avatar : "/media/default_avatar.png")} />
+                                    <div className="learn-course-author-username">{course.author.user.username}</div>
+                                </div>
                                 Language: {LANGUAGES.find(lang => lang[0] === course.language)?.[2]} {LANGUAGES.find(lang => lang[0] === course.language)?.[1]} <br />
                                 Duration: {course.duration} hours <br />
-                                Last updated {timeAgo(new Date(course.last_updated))} <br />
+                                {/* Last updated {timeAgo(new Date(course.last_updated))} <br /> */}
 
                             </span>
                         </div>
