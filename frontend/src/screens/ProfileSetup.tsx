37b4d6d5f07b31045fff898add5374526ea25a0b
Bartosz Kowalski <01168892@pw.edu.pl>
2025-01-14 16:49:20 +0100

    ProfileSetup CSS + respo

--- frontend/src/screens/ProfileSetup.tsx
+++ frontend/src/screens/ProfileSetup.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import '../App.css';
 import { sendUserBackToLoginPageIfNotLoggedIn } from '../functions';
+import "../styles/ProfileSetup.css";
 
 export default function ProfileSetupPage() {
     const [firstName, setFirstName] = useState<string>("");
@@ -10,6 +11,30 @@ export default function ProfileSetupPage() {
     const [instagram, setInstagram] = useState<string>("");
     const [tiktok, setTiktok] = useState<string>("");
     const [linkedin, setLinkedin] = useState<string>("");
+
+    const [account, setAccount] = useState<Account>();
+
+    const fetchAccount = async () => {
+        const response = await fetch("http://127.0.0.1:8000/api/profile/me", {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Token ${localStorage.getItem("token")}`,
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setFirstName(data.user.first_name);
+                setLastName(data.user.last_name);
+                setBio(data.bio);
+                setFacebook(data.socials.facebook);
+                setInstagram(data.socials.instagram);
+                setTiktok(data.socials.tiktok);
+                setLinkedin(data.socials.linkedin);
+                setAccount(data);
+            });
+    }
 
     const handleSetup = async () => {
         const response = await fetch("http://127.0.0.1:8000/api/profile/setup", {
@@ -38,39 +63,54 @@ export default function ProfileSetupPage() {
 
     useEffect(() => {
         sendUserBackToLoginPageIfNotLoggedIn();
+        fetchAccount();
     }, []);
 
+    if (!account) {
+        return (
+            <>Loading...</>
+        );
+    };
+
     return (
-        <>
-            hello, setup your profile
+        <div id="create-course-main" className="text-align-center">
+            <h1>Hello, <span className="blue">{account.user.username}</span></h1>
             <br />
-            <form onSubmit={(e) => e.preventDefault()}>
-                <br />
-                first name:
-                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                <br />
-                last name:
-                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                <br />
-                bio:
-                <input type="text" value={bio} onChange={(e) => setBio(e.target.value)} />
-                <br />
-                fb:
-                <input type="text" value={facebook} onChange={(e) => setFacebook(e.target.value)} />
-                <br />
-                ig:
-                <input type="text" value={instagram} onChange={(e) => setInstagram(e.target.value)} />
-                <br />
-                tt:
-                <input type="text" value={tiktok} onChange={(e) => setTiktok(e.target.value)} />
-                <br />
-                li:
-                <input type="text" value={linkedin} onChange={(e) => setLinkedin(e.target.value)} />
-                <br />
-                <button type="button" onClick={handleSetup}>Save</button>
-            </form>
+            <div className="create-course-label-box">
+                First name
+            </div>
+            <input className="create-course-input-text" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            <div className="create-course-label-box">
+                Last name
+            </div>
+            <input className="create-course-input-text" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            <div className="create-course-label-box">
+                Bio
+            </div>
+            <input className="create-course-input-text" type="text" value={bio} onChange={(e) => setBio(e.target.value)} />
+            <div className="create-course-label-box">
+                Facebook full link
+            </div>
+            <input className="create-course-input-text" type="text" value={facebook} onChange={(e) => setFacebook(e.target.value)} />
+            <div className="create-course-label-box">
+                Instagram full link
+            </div>
+            <input className="create-course-input-text" type="text" value={instagram} onChange={(e) => setInstagram(e.target.value)} />
+            <div className="create-course-label-box">
+                Tiktok full link
+            </div>
+            <input className="create-course-input-text" type="text" value={tiktok} onChange={(e) => setTiktok(e.target.value)} />
+            <div className="create-course-label-box">
+                LinkedIn full link
+            </div>
+            <input className="create-course-input-text" type="text" value={linkedin} onChange={(e) => setLinkedin(e.target.value)} />
             <br />
-            <a href="/profile/me">Skip for now</a>
-        </>
+            <button className="create-course-step-button" type="button" onClick={handleSetup}>Save</button>
+            <a href="/profile/me">
+                <button className="create-course-step-button">
+                    Skip for now
+                </button>
+            </a>
+        </div>
     );
 }
